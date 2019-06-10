419fcc77757ecda83c21cf0545ce3f335d6987ba
Vytaly Yanpolsky <vyanpolsky@neoflex.ru>
2019-06-10 10:18:39 +0300

    Созданы новые классы Rectangle и Square

--- sandbox/src/main/java/ru/stqa/pft/sandbox/Hello.java
+++ sandbox/src/main/java/ru/stqa/pft/sandbox/Hello.java
@@ -1,8 +1,21 @@
 package ru.stqa.pft.sandbox;
 
-public class Hello{
-	
+public class Hello {
+
 	public static void main(String[] args) {
-		System.out.println("Hello, world!");
+
+		hello("World");
+		hello("User");
+		hello("Vitaly");
+
+		Square s = new Square(5);
+		System.out.println("Площадь квадрата со стороной " + s.l + " = " + s.area());
+
+		Rectangle r = new Rectangle(4,6);
+		System.out.println("Площадь со стороной " + r.a + " и " + r.b + " = " + r.area());
+	}
+
+	public static void hello (String somebody) {
+		System.out.println("Hello, " + somebody + "!");
 	}
 }
